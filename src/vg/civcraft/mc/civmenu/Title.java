da86beb3f2cf9972cb72b3294d3747b0deafdba0
Maxopoly <maxopoly@civcraft.co>
2016-06-11 02:35:28 +0200

    Update to 1.10

--- src/vg/civcraft/mc/civmenu/Title.java
+++ src/vg/civcraft/mc/civmenu/Title.java
@@ -1,11 +1,11 @@
 package vg.civcraft.mc.civmenu;
 
-import org.bukkit.craftbukkit.v1_9_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_10_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
-import net.minecraft.server.v1_9_R1.IChatBaseComponent;
-import net.minecraft.server.v1_9_R1.PacketPlayOutTitle;
-import net.minecraft.server.v1_9_R1.PlayerConnection;
+import net.minecraft.server.v1_10_R1.IChatBaseComponent;
+import net.minecraft.server.v1_10_R1.PacketPlayOutTitle;
+import net.minecraft.server.v1_10_R1.PlayerConnection;
 
 public class Title {
 	private String title;
